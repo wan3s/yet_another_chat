f9d50eae7c9aa3b21ecc1f69939cc92ba306f060
KorotkovBS <only77nt@gmail.com>
2021-05-05 15:37:28 +0300

    add wheel

--- module/__init__.py
+++ module/__init__.py
@@ -1,20 +1,4 @@
 from .func import say_hello
 
-"""
-.. module:: module
-    :platform: Unix, Windows
-    :synopsis: A useful module indeed.
-
-.. moduleauthor:: KorotkovBS
-"""
-
-def main():
-	"""
-	Dumb function.
-	
-	:return: nothing
-	"""
-	print("Module is here!")
-
 if __name__ == "__main__":
 	main()
